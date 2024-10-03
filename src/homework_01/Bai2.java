a25ab87674cc4eeced6f084a82c8c1a002f4ffa6
tranphuockhoa <161118784+tranphuockhoa@users.noreply.github.com>
2024-10-03 23:02:27 +0700

    Add files via upload

---
+++ src/homework_01/Bai2.java
@@ -0,0 +1,57 @@
+package homework_01;
+
+import java.util.Scanner;
+
+public class Bai2 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);	
+		int N = 0;
+		int Max_Input = 5;
+		boolean validInput = false;
+		
+		for(int i = 0; i < Max_Input; i++) {
+			System.out.print("Nhap so nguyen N = ");
+			String input = sc.nextLine();
+			
+			N = kiemTraSoNguyenDuong(input);
+			if(N != -1) {// cho truong hop hop le
+				validInput = true;
+				break;
+			} else {// cho truong hop ko hop le
+				System.out.println("Chi duoc nhap so nguyen!!");
+				if(i < Max_Input -1) {
+					System.out.println("Vui long nhap lai.");
+				}
+			}
+		}
+		if (validInput) {
+			if (kiemTraLuyThuaCua2(N)) {
+				System.out.println("true");
+			} else {
+				System.out.println("false");
+			}
+		
+		}
+		sc.close();
+		
+	}
+	public static int kiemTraSoNguyenDuong(String input) {
+        try {
+            int number = Integer.parseInt(input);
+            if (number > 0) {
+                return number;
+            } else {
+                return -1;
+            }
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
+	 public static boolean kiemTraLuyThuaCua2(int number) {
+	        if (number <= 0) {
+	            return false;
+	        }
+	        return (number & (number - 1)) == 0;
+	    }
+}
