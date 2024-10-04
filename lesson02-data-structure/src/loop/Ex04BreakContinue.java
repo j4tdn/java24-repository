a728fa162cf899736bc5049dfd666d7498a31460
TienSon <sontn1pm2@gmail.com>
2024-10-04 20:47:02 +0700

    lesson02-data-structure 04.10.2-2024

---
+++ lesson02-data-structure/src/loop/Ex04BreakContinue.java
@@ -0,0 +1,25 @@
+package loop;
+
+public class Ex04BreakContinue {
+	
+	public static void main(String[] args) {
+		//switch case break
+		//loop break continue
+		for (int i=1;i<=10;i++) {
+			if ( i % 5 == 0 ) {
+				break;
+			}
+			System.out.println(i+ " ");
+		}
+		System.out.println("\n==============\n");
+		for (int i=1;i<=10;i++) {
+			if ( i % 5 == 0 ) {
+				// Kết thúc sớm vòng lặp hiện tại rồi
+				//nhảy sang vòng lặp tiếp theo
+				continue;
+			}
+			System.out.println(i+ " ");
+		}
+	}
+
+}
