6ea5cb343177e90184062d664f9cd9934350ce25
Phuoc Dung <mphuocdung97@gmail.com>
2024-10-05 20:55:51 +0700

    lesson02-personal-update-05.10.2024

--- lesson02-personal/src/view/Ex01CheckMultipleOf2.java
+++ lesson02-personal/src/view/Ex01CheckMultipleOf2.java
@@ -2,6 +2,8 @@ package view;
 
 import java.util.Scanner;
 
+import utils.Ex01Utils;
+
 public class Ex01CheckMultipleOf2 {
 
 	/*
@@ -11,50 +13,16 @@ public class Ex01CheckMultipleOf2 {
 	 */
 
 	public static void main(String[] args) {
-
 		Scanner ip = new Scanner(System.in);
-		String text = "";
-		int wrongtimes = 0;
-
-		while (true) {
-			System.out.println("Nhập vào số nguyên dương N");
-			text = ip.nextLine();
-			if (isNumber(text)) {
-				break;
-			}
-			wrongtimes++;
-			System.out.println("Không hợp lệ, sai lần thứ " + wrongtimes);
-			
-			if(wrongtimes == 5) {
-				System.out.println("Sai quá 5 lần, thoát chương trình");
-				System.exit(0);
-			}
-		}
 		
-		int n = Integer.parseInt(text);
-		if( n % 2 == 0 ) {
-			System.out.println(text + " là bội của 2 ");
+		int n = Ex01Utils.testInput(5, 0, "Nhập vào số nguyên N ");
+		if(n % 2 == 0) {
+			System.out.println(n + " là bội của 2 ");
 		} else {
-			System.out.println(text + " không phải là bội của 2 ");
+			System.out.println(n + " không phải là bội của 2 ");
 		}
+		System.out.println("Kết thúc chương trình");
 		ip.close();
-	}
-
-	private static boolean isNumber(String text) {
-
-		if (text == null || text.isEmpty()) {
-			return false;
-		}
-
-		for (int i = 0; i < text.length(); i++) {
-			char letter = text.charAt(i);
-			if (letter < '0' || letter > '9') {
-				return false;
-			}
-		}
-
-		return true;
-
 	}
 
 }
