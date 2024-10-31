c3afade955d888766c0457bb1eeb2bf7f5c7bf66
Hkhanhs <khanhdz211105@gmail.com>
2024-10-31 22:07:14 +0700

    lesson04-oop-features 31.10.2024

---
+++ lesson04-oop-features/src/inheritance/mutiple/Child.java
@@ -0,0 +1,18 @@
+package inheritance.mutiple;
+
+public class Child extends Father implements GrandFather, GrandMother {
+	
+	// JDK 1.0 1.1 1.7 8 9 10 11 12 13
+	// ES5, ES6 ES2019 ES2020
+	
+	@Override
+	public void building() {
+		
+	}
+	
+	@Override
+	public void coding() {
+		
+	}
+	
+}
---
+++ lesson04-oop-features/src/inheritance/mutiple/Ex01MultipleInheritanceDemo.java
@@ -0,0 +1,13 @@
+package inheritance.mutiple;
+
+public class Ex01MultipleInheritanceDemo {
+	
+	public static void main(String[] args) {
+		
+		Child c1 = new Child();
+		//c1.running();
+		// c1.playBadminton();
+		// c1.cook();
+	}
+	
+}
