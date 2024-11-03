32ebce4a0f6d685ff848d3d9e4288ee5f3b8522b
phuy <phuchuy1212003@gmail.com>
2024-11-03 14:50:35 +0700

    lesson05-phuchuy add Ex03 11.03.2024

--- lesson05-phuchuy/src/view/Ex02SpecialNumber.java
+++ lesson05-phuchuy/src/view/Ex02SpecialNumber.java
@@ -1,5 +1,21 @@
 package view;
 
 public class Ex02SpecialNumber {
+	public static void main(String[] args) {
+
+		System.out.println(isSpecialNumber(6, 2));
+
+	}
+
+	private static boolean isSpecialNumber(int number, int n) {
+		int sum = 0;
+		for (int i = 1; i <= n; i++) {
+			sum = sum + i;
+			if (sum == number) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
