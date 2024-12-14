1e4540b37d6059ecc916d0b8bcaf4a7e0580b7d3
khoanc2304 <1fvhtkhoa@gmail.com>
2024-12-14 22:28:36 +0700

    lesson00-design-pattern 13.12.2024

---
+++ lesson00-design-pattern/src/singleton/Ex01SingletonDemo.java
@@ -0,0 +1,19 @@
+package singleton;
+
+public class Ex01SingletonDemo {
+
+	public static void main(String[] args) {
+		RemoteDate rd1 = RemoteDate.getInstance();
+		rd1.setDayOfYear(48);
+		System.out.println("rd1 hash --> " + System.identityHashCode(rd1));
+		System.out.println("rd1 value --> " + rd1);
+		
+		System.out.println("\n==============\n");
+		
+		RemoteDate rd2 = RemoteDate.getInstance();
+		System.out.println("rd2 hash --> " + System.identityHashCode(rd2));
+		System.out.println("rd2 value --> " + rd2);
+
+	}
+	
+}
