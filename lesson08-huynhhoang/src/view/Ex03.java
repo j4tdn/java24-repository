b18b1ebb846d98e8e97e26795a12f59366f233ab
huynh hoang <huynhhoang712003@gmail.com>
2024-12-31 17:07:21 +0700

    lesson08-huynhhoang 31.12.2024

---
+++ lesson08-huynhhoang/src/view/Ex03.java
@@ -0,0 +1,21 @@
+package view;
+
+import java.util.Scanner;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		String text = "Em có yêu anh không anh để anh biết còn chờ\r\n"
+				+ "Em xin lỗi em đã có thái độ không đúng với anh";
+		System.out.println("Chuoi sau khi loai bo: \n"+convert(text));
+	}
+	private static String convert(String str) {
+        str = str.replaceAll("à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ", "a");
+        str = str.replaceAll("è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ", "e");
+        str = str.replaceAll("ì|í|ị|ỉ|ĩ", "i");
+        str = str.replaceAll("ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ", "o");
+        str = str.replaceAll("ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ", "u");
+        str = str.replaceAll("ỳ|ý|ỵ|ỷ|ỹ", "y");
+        str = str.replaceAll("đ", "d");
+        return str;
+    }
+}
