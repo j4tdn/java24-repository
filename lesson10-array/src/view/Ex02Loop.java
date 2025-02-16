4bf3377e09f6fc6d3692d17615943c26512e5369
tranphuockhoa <tranphuockhoa@dtu.edu.vn>
2025-02-16 12:45:36 +0700

    lesson10-array 14.2.2025 -2

--- lesson10-array/src/view/Ex02Loop.java
+++ lesson10-array/src/view/Ex02Loop.java
@@ -1,29 +1,23 @@
 package view;
 
-import java.util.Arrays;
+public class Ex02Loop {
 
-import bean.Item;
-
-public class Ex01Initial {
-	
 	public static void main(String[] args) {
-		// Khai báo, khởi tạo mảng
-		// Trước khi tạo mảng --> biết được mảng đó sẽ lưu tối đa bao nhiêu phần tử
-		int[] digits = new int[8];
-		String[] sequences = {"hello", "welcome", "language"};
-		double[] avgPoints = generatePoints();
-		Item[] items = new Item[4];
+
+		int[] numbers = { 1, 2, 3, 4, 5 };
 		
-		System.out.printf("digits %s\n", Arrays.toString(digits));
-		System.out.printf("sequences %s\n", Arrays.toString(sequences));
-		System.out.printf("avgPoints %s\n", Arrays.toString(avgPoints));
-		items[0] = new Item();
-		items[0].setSalesPrice(999d);
-		System.out.printf("items %s\n", Arrays.toString(items));
+		// for index
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.print(numbers[i] + " ");
+		}
+		System.out.println("\n");
+		
+		
+		// for each
+		for (int number: numbers) {
+			System.out.print(number + " ");
+		}
+
 	}
-	
-	private static double[] generatePoints() {
-		return new double[] {2,8d, 8.2d, 8.8d, 12,6d};
-	}
-	
+
 }
