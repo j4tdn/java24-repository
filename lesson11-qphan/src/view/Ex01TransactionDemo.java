7702f126b8867582a9072322b4e2cf9ca3c1a0a7
Quyen Phan <Quyen.Phan@mgm-tp.com>
2025-03-23 14:17:32 +0700

    lesson11-qphan 23.03.2025

---
+++ lesson11-qphan/src/view/Ex01TransactionDemo.java
@@ -0,0 +1,32 @@
+package view;
+
+import model.DataModel;
+
+public class Ex01TransactionDemo {
+	
+	public static void main(String[] args) {
+		
+		var transactions = DataModel.getTransactions();
+		
+		// 1. Find all transactions in the year 2011 and sort them by value (small to high).
+		
+		// 2. Find all transactions have value greater than 300 and sort them by trader’s city
+		
+		// 3. What are all the unique cities where the traders work?
+		
+		// 4. Find all traders from Cambridge and sort them by name desc.
+		
+		// 5. Return a string of all traders’ names sorted alphabetically.
+		
+		// 6. Are any traders based in Milan?
+		
+		// 7. Count the number of traders in Milan.
+		
+		// 8. Print all transactions’ values from the traders living in Cambridge.
+		
+		// 9. What’s the highest value of all the transactions?
+		
+		// 10. Find the transaction with the smallest value.
+	}
+	
+}
