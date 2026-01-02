220a97793d10a8a7d7e1f0d4d6d709fccb0a2c2e
Quyen Phan <Quyen.Phan@mgm-tp.com>
2026-01-02 21:19:28 +0700

    lesson17-database-connection 02.01.2026

---
+++ lesson17-database-connection/src/main/java/bean/ItemGroup.java
@@ -0,0 +1,70 @@
+package bean;
+
+import java.util.Objects;
+
+public class ItemGroup {
+	
+	private Integer id;
+	private String name;
+	private Boolean status;
+	
+	public ItemGroup() {
+	}
+
+	public ItemGroup(String name, Boolean status) {
+		this.name = name;
+		this.status = status;
+	}
+	
+	public ItemGroup(Integer id, String name, Boolean status) {
+		this.id = id;
+		this.name = name;
+		this.status = status;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Boolean getStatus() {
+		return status;
+	}
+
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof ItemGroup that)) {
+			return false;
+		}
+		
+		return getId() == that.getId();
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(getId());
+	}
+
+	@Override
+	public String toString() {
+		return "ItemGroup [id=" + id + ", name=" + name + ", status=" + status + "]";
+	}
+}
---
+++ lesson17-database-connection/src/main/java/view/Ex02ItemGroupView.java
@@ -0,0 +1,62 @@
+package view;
+
+import java.util.List;
+
+import bean.ItemGroup;
+import service.ItemGroupService;
+import service.ItemGroupServiceImpl;
+import utils.IoUtils;
+
+public class Ex02ItemGroupView {
+	
+	private static ItemGroupService itemGroupService;
+	
+	static {
+		itemGroupService = new ItemGroupServiceImpl();
+	}
+	
+	public static void main(String[] args) {
+		// itemGroupService.save(new ItemGroup("Loại Hàng X1", Boolean.TRUE));
+		// itemGroupService.update(new ItemGroup(10, "Loại Hàng X10", Boolean.FALSE));
+		// itemGroupService.saveOrUpdate(new ItemGroup(11, "Loại Hàng X11", Boolean.TRUE));
+		
+		itemGroupService.saveGroups(List.of(
+			new ItemGroup("Loại Hàng 14", true),
+			new ItemGroup("Loại Hàng 15", false),
+			new ItemGroup("Loại Hàng 16", true)
+		));
+		
+		IoUtils.generate(
+			"1. Liệt kê các loại hàng trong hệ thống", 
+			itemGroupService.getAll()
+		);
+		
+		IoUtils.generate(
+			"2. Liệt kê loại hàng theo mã loại", 
+			itemGroupService.get(22)
+		);
+		
+		// 3. Liệt kê loại hàng theo tên loại
+		// - Statement: Tìm hiểu lỗi sql injection
+		// - PreparedStatement: Xử lý lỗi
+		
+		// 4. Liệt kê các mặt hàng(kèm thông tin loại hàng)
+		// Yêu cầu: Lấy tất cả các cột
+		
+		// 5. Liệt kê các loại hàng(kèm danh sách mặt hàng của mỗi loại)
+		// Yêu cầu: Lấy tất cả các cột
+		
+		// 6. Liệt kê, đếm số lượng mặt hàng của từng loại hàng
+		// Thông tin cần lấy: MaLH, TenLH, TongSoLuongMatHang, ChiTiet(TenMatHang, KichCo, SoLuong)
+		
+		// 7. Liệt kê mặt hàng có số lượng nhiều nhất trong hệ thống
+		// Yêu cầu: TenMH, SoLuong
+		
+		// 8. Truy vấn dữ liệu liên quan Date, Time
+		
+		// 9. Thêm mới khách hàng vào hệ thống(MD5)
+		
+		// 10. Bài toán @Transactional
+	}
+	
+}
